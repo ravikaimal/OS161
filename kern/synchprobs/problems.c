602704a8a2153d00ea7f47a23776e7e244c825ce
rpirakil <rpirakil@buffalo.edu>
2015-02-12 19:10:55 -0500

    Stoplight implementation test

--- kern/synchprobs/problems.c
+++ kern/synchprobs/problems.c
@@ -58,16 +58,32 @@ struct whale{
 };
 
 struct whale *whale ;
-struct lock *lock_0;
-struct lock *lock_1;
-struct lock *lock_2;
-struct lock *lock_3;
+
+struct intersection{
+	volatile int quad0WaitCount ;
+	volatile int quad1WaitCount ;
+	volatile int quad2WaitCount ;
+	volatile int quad3WaitCount ;
+
+	struct cv *cv ;
+
+	struct lock *lock_0;
+	struct lock *lock_1;
+	struct lock *lock_2;
+	struct lock *lock_3;
+};
+
+struct intersection *intersection ;
+
 void get_lock(unsigned long);
 void give_lock(unsigned long);
-
+int getIntersectionWaitCount(unsigned long);
+void incrementIntersectionWaitCount(unsigned long);
+void decrementIntersectionWaitCount(unsigned long);
+struct lock* getIntersectionLock(unsigned long);
 
 void whalemating_init() {
-  whale = kmalloc(sizeof(struct whale));
+	whale = kmalloc(sizeof(struct whale));
 	whale->maleCount = 0 ;
 	whale->femaleCount = 0 ;
 	whale->matchMakerCount = 0 ;
@@ -75,14 +91,14 @@ void whalemating_init() {
 	whale->malelock = lock_create("malelock") ;
 	whale->femalelock = lock_create("femalelock") ;
 	whale->matchMakerlock = lock_create("matchMakerlock") ;
-  return;
+	return;
 }
 
 // 20 Feb 2012 : GWA : Adding at the suggestion of Nikhil Londhe. We don't
 // care if your problems leak memory, but if you do, use this to clean up.
 
 void whalemating_cleanup() {
-  whale->maleCount = 0 ;
+	whale->maleCount = 0 ;
 	whale->femaleCount = 0 ;
 	whale->matchMakerCount = 0 ;
 	cv_destroy(whale->cv) ;
@@ -90,80 +106,80 @@ void whalemating_cleanup() {
 	lock_destroy(whale->femalelock) ;
 	lock_destroy(whale->matchMakerlock) ;
 	kfree(whale) ;
-  return;
+	return;
 }
 
 void
 male(void *p, unsigned long which)
 {
 	struct semaphore * whalematingMenuSemaphore = (struct semaphore *)p;
-  (void)which;
-  
-  male_start();
-  lock_acquire(whale->malelock) ;
-  whale->maleCount++ ;
+	(void)which;
 
-  while(whale->femaleCount == 0 || whale->matchMakerCount == 0  ){
-	cv_wait(whale->cv,whale->malelock) ;
-  }
-  cv_broadcast(whale->cv,whale->malelock) ;
-  male_end();
-  whale->maleCount-- ;
-  lock_release(whale->malelock) ;
+	male_start();
+	lock_acquire(whale->malelock) ;
+	whale->maleCount++ ;
 
-  // 08 Feb 2012 : GWA : Please do not change this code. This is so that your
-  // whalemating driver can return to the menu cleanly.
-  V(whalematingMenuSemaphore);
-  return;
+	while(whale->femaleCount == 0 || whale->matchMakerCount == 0  ){
+		cv_wait(whale->cv,whale->malelock) ;
+	}
+	cv_broadcast(whale->cv,whale->malelock) ;
+	male_end();
+	whale->maleCount-- ;
+	lock_release(whale->malelock) ;
+
+	// 08 Feb 2012 : GWA : Please do not change this code. This is so that your
+	// whalemating driver can return to the menu cleanly.
+	V(whalematingMenuSemaphore);
+	return;
 }
 
 void
 female(void *p, unsigned long which)
 {
 	struct semaphore * whalematingMenuSemaphore = (struct semaphore *)p;
-  (void)which;
-  
-  female_start();
-lock_acquire(whale->femalelock) ;
-  whale->femaleCount++ ;
+	(void)which;
 
-  while(whale->maleCount == 0 || whale->matchMakerCount == 0){
+	female_start();
+	lock_acquire(whale->femalelock) ;
+	whale->femaleCount++ ;
 
-	  cv_wait(whale->cv,whale->femalelock) ;
+	while(whale->maleCount == 0 || whale->matchMakerCount == 0){
 
-  }
-  cv_broadcast(whale->cv,whale->femalelock) ;
-  female_end();
-  whale->femaleCount-- ;
-  lock_release(whale->femalelock) ;
-  
-  // 08 Feb 2012 : GWA : Please do not change this code. This is so that your
-  // whalemating driver can return to the menu cleanly.
-  V(whalematingMenuSemaphore);
-  return;
+		cv_wait(whale->cv,whale->femalelock) ;
+
+	}
+	cv_broadcast(whale->cv,whale->femalelock) ;
+	female_end();
+	whale->femaleCount-- ;
+	lock_release(whale->femalelock) ;
+
+	// 08 Feb 2012 : GWA : Please do not change this code. This is so that your
+	// whalemating driver can return to the menu cleanly.
+	V(whalematingMenuSemaphore);
+	return;
 }
 
 void
 matchmaker(void *p, unsigned long which)
 {
 	struct semaphore * whalematingMenuSemaphore = (struct semaphore *)p;
-  (void)which;
-  
-  matchmaker_start();
-  lock_acquire(whale->matchMakerlock) ;
-  whale->matchMakerCount++ ;
-  while(whale->maleCount == 0 || whale->femaleCount == 0){
-	  cv_wait(whale->cv,whale->matchMakerlock) ;
-  }
-  cv_broadcast(whale->cv,whale->matchMakerlock) ;
-  matchmaker_end();
-  whale->matchMakerCount-- ;
-  lock_release(whale->matchMakerlock) ;
-  
-  // 08 Feb 2012 : GWA : Please do not change this code. This is so that your
-  // whalemating driver can return to the menu cleanly.
-  V(whalematingMenuSemaphore);
-  return;
+	(void)which;
+
+	matchmaker_start();
+	lock_acquire(whale->matchMakerlock) ;
+	whale->matchMakerCount++ ;
+	while(whale->maleCount == 0 || whale->femaleCount == 0){
+		cv_wait(whale->cv,whale->matchMakerlock) ;
+	}
+	cv_broadcast(whale->cv,whale->matchMakerlock) ;
+	matchmaker_end();
+	whale->matchMakerCount-- ;
+	lock_release(whale->matchMakerlock) ;
+
+	// 08 Feb 2012 : GWA : Please do not change this code. This is so that your
+	// whalemating driver can return to the menu cleanly.
+	V(whalematingMenuSemaphore);
+	return;
 }
 
 /*
@@ -198,97 +214,205 @@ matchmaker(void *p, unsigned long which)
 // the top of the corresponding driver code.
 
 void stoplight_init() {
-  lock_0=lock_create("Quadrant 0 Lock");
-  lock_1=lock_create("Quadrant 1 Lock");
-  lock_2=lock_create("Quadrant 2 Lock");
-  lock_3=lock_create("Quadrant 3 Lock");
-  return;
+
+	intersection = kmalloc(sizeof(struct intersection));
+
+	intersection->lock_0=lock_create("Quadrant 0 Lock");
+	intersection->lock_1=lock_create("Quadrant 1 Lock");
+	intersection->lock_2=lock_create("Quadrant 2 Lock");
+	intersection->lock_3=lock_create("Quadrant 3 Lock");
+
+	intersection->quad0WaitCount = 0 ;
+	intersection->quad1WaitCount = 0 ;
+	intersection->quad2WaitCount = 0 ;
+	intersection->quad3WaitCount = 0 ;
+
+	intersection->cv = cv_create("intersection cv") ;
+	return;
 }
 
 // 20 Feb 2012 : GWA : Adding at the suggestion of Nikhil Londhe. We don't
 // care if your problems leak memory, but if you do, use this to clean up.
 
 void stoplight_cleanup() {
-  lock_destroy(lock_0);
-  lock_destroy(lock_1);
-  lock_destroy(lock_2);
-  lock_destroy(lock_3);
-  return;
+	intersection->quad0WaitCount = 0 ;
+	intersection->quad1WaitCount = 0 ;
+	intersection->quad2WaitCount = 0 ;
+	intersection->quad3WaitCount = 0 ;
+
+	cv_destroy(intersection->cv) ;
+
+	lock_destroy(intersection->lock_0);
+	lock_destroy(intersection->lock_1);
+	lock_destroy(intersection->lock_2);
+	lock_destroy(intersection->lock_3);
+
+	kfree(intersection) ;
+
+	return;
 }
 
 void
 gostraight(void *p, unsigned long direction)
 {
 	struct semaphore * stoplightMenuSemaphore = (struct semaphore *)p;
-  (void)direction;
-  get_lock(direction);
-  inQuadrant(direction);
-  give_lock(direction);
-  get_lock((direction+3)%4);
-  inQuadrant((direction+3)%4);
-  leaveIntersection();
-  give_lock((direction+3)%4);
-  V(stoplightMenuSemaphore);
-  return;
+	(void)direction;
+
+	get_lock(direction) ;
+	while (getIntersectionWaitCount(direction) > 0 &&  getIntersectionWaitCount((direction+3)%4) > 0)
+	{
+		incrementIntersectionWaitCount(direction) ;
+		cv_wait(intersection->cv,getIntersectionLock(direction)) ;
+		decrementIntersectionWaitCount(direction) ;
+	}
+
+	get_lock((direction+3)%4);
+
+	inQuadrant(direction);
+	inQuadrant((direction+3)%4);
+	cv_broadcast(intersection->cv,getIntersectionLock(direction)) ;
+	give_lock(direction);
+
+	leaveIntersection();
+
+	give_lock((direction+3)%4);
+
+	V(stoplightMenuSemaphore);
+	return;
 }
+
+
 
 void
 turnleft(void *p, unsigned long direction)
 {
 	struct semaphore * stoplightMenuSemaphore = (struct semaphore *)p;
-  (void)direction;
-  get_lock(direction);
-  inQuadrant(direction);
-  give_lock(direction);
-  get_lock((direction+3)%4);
-  inQuadrant((direction+3)%4);
-  give_lock((direction+3)%4);
-  get_lock((direction+2)%4);
-  inQuadrant(direction+2%4);
-  leaveIntersection();
-  give_lock((direction+2)%4);
-  // 08 Feb 2012 : GWA : Please do not change this code. This is so that your
-  // stoplight driver can return to the menu cleanly.
-  V(stoplightMenuSemaphore);
-  return;
+	(void)direction;
+	get_lock(direction);
+
+	while (getIntersectionWaitCount(direction) > 0 &&  getIntersectionWaitCount((direction+3)%4) > 0 && getIntersectionWaitCount((direction+2)%4) > 0)
+	{
+		incrementIntersectionWaitCount(direction) ;
+		cv_wait(intersection->cv,getIntersectionLock(direction)) ;
+		decrementIntersectionWaitCount(direction) ;
+	}
+
+	get_lock((direction+3)%4);
+	get_lock((direction+2)%4);
+
+	inQuadrant(direction);
+
+	inQuadrant((direction+3)%4);
+	cv_broadcast(intersection->cv,getIntersectionLock(direction)) ;
+	give_lock(direction);
+
+	inQuadrant((direction+2)%4);
+	give_lock((direction+3)%4);
+
+	leaveIntersection();
+
+	give_lock((direction+2)%4);
+
+	// 08 Feb 2012 : GWA : Please do not change this code. This is so that your
+	// stoplight driver can return to the menu cleanly.
+	V(stoplightMenuSemaphore);
+	return;
 }
 
 void
 turnright(void *p, unsigned long direction)
 {
 	struct semaphore * stoplightMenuSemaphore = (struct semaphore *)p;
-  (void)direction;
-  get_lock(direction);
-  inQuadrant(direction);
-  leaveIntersection();
-  give_lock(direction);
+	(void)direction;
 
-  // 08 Feb 2012 : GWA : Please do not change this code. This is so that your
-  // stoplight driver can return to the menu cleanly.
-  V(stoplightMenuSemaphore);
-  return;
+	get_lock(direction);
+
+	while (getIntersectionWaitCount(direction) > 0)
+	{
+		incrementIntersectionWaitCount(direction) ;
+		cv_wait(intersection->cv,getIntersectionLock(direction)) ;
+		decrementIntersectionWaitCount(direction) ;
+	}
+
+	inQuadrant(direction);
+	leaveIntersection();
+	cv_broadcast(intersection->cv,getIntersectionLock(direction)) ;
+	give_lock(direction);
+
+	// 08 Feb 2012 : GWA : Please do not change this code. This is so that your
+	// stoplight driver can return to the menu cleanly.
+	V(stoplightMenuSemaphore);
+	return;
 }
 
 void get_lock(unsigned long direction)
 {
-  switch(direction)
-  {
-        case 0 : lock_acquire(lock_0); break;
-        case 1 : lock_acquire(lock_1); break;
-        case 2 : lock_acquire(lock_2); break;
-	case 3 : lock_acquire(lock_3); break;
-  }
+	switch(direction)
+	{
+	case 0 : lock_acquire(intersection->lock_0); break;
+	case 1 : lock_acquire(intersection->lock_1); break;
+	case 2 : lock_acquire(intersection->lock_2); break;
+	case 3 : lock_acquire(intersection->lock_3); break;
+	}
 
 }
 
 void give_lock(unsigned long direction)
 {
-  switch(direction)
-  {
-        case 0 : lock_release(lock_0); break;
-        case 1 : lock_release(lock_1); break;
-        case 2 : lock_release(lock_2); break;
-        case 3 : lock_release(lock_3); break;
-  }
+	switch(direction)
+	{
+	case 0 : lock_release(intersection->lock_0); break;
+	case 1 : lock_release(intersection->lock_1); break;
+	case 2 : lock_release(intersection->lock_2); break;
+	case 3 : lock_release(intersection->lock_3); break;
+	}
 
+}
+
+int getIntersectionWaitCount(unsigned long direction)
+{
+	switch(direction)
+	{
+	case 0:return intersection->quad0WaitCount ;
+	case 1:return intersection->quad1WaitCount ;
+	case 2:return intersection->quad2WaitCount ;
+	case 3:return intersection->quad3WaitCount ;
+	}
+
+	return 0 ;
+}
+
+void incrementIntersectionWaitCount(unsigned long direction)
+{
+	switch(direction)
+	{
+	case 0:intersection->quad0WaitCount++ ; break ;
+	case 1:intersection->quad1WaitCount++ ; break ;
+	case 2:intersection->quad2WaitCount++ ; break ;
+	case 3:intersection->quad3WaitCount++ ; break ;
+	}
+}
+
+void decrementIntersectionWaitCount(unsigned long direction)
+{
+	switch(direction)
+	{
+	case 0:intersection->quad0WaitCount-- ; break ;
+	case 1:intersection->quad1WaitCount-- ; break ;
+	case 2:intersection->quad2WaitCount-- ; break ;
+	case 3:intersection->quad3WaitCount-- ; break ;
+	}
+}
+
+struct lock* getIntersectionLock(unsigned long direction)
+{
+	switch(direction)
+	{
+	case 0:return intersection->lock_0 ;
+	case 1:return intersection->lock_1 ;
+	case 2:return intersection->lock_2 ;
+	case 3:return intersection->lock_3 ;
+	}
+
+	return NULL;
 }
